f6507bf5de600db2fd3988dd5c0c65330e7ce270
Abrin Era <110847229+abrinera@users.noreply.github.com>
2025-07-02 02:15:10 +0600

    RSA encryption and decryption in python

---
+++ RSA_encryption_decryption.py
@@ -0,0 +1,45 @@
+x=int(input("Enter (1) for encryption and (2) for decryption: "))
+e=int(input("Enter e: "))
+n=int(input("Enter n: "))
+
+if x==1:
+    m= int(input("Enter message: "))
+    ciper= (m**e)%n
+    print(f"The encryption of {m} is = {ciper}")    
+
+elif x==2:
+     c= int(input("Enter ciper: ")) 
+     def is_prime(num):
+        if num==1:
+            return True
+        else:
+            for i in range(2,num):
+                if num%i==0:
+                    return False
+            return True  
+        
+     for p in range(2, n):
+        if n % p == 0:
+            q = n // p
+            if is_prime(p) and is_prime(q):
+                print(f"p={p}, q={q} are primes.")
+                break
+                 
+     fie= (p-1)*(q-1)
+     print(f"the fie ={fie}")
+     
+     for k in range(1,e+1):
+         d= (1+ k*fie)/e
+         if d==int(d):
+             print(f"d = {d}")
+             break
+         
+     message= (c**d)%n
+     print(f"The decryption of {c} is = {message}")    
+                 
+else:
+    print("Please enter correct option")    
+    
+    
+    
+  
